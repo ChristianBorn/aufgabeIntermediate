19353215a014642ac2f976b05b83fb6515db7988
christianborn <chrisborn@hotmail.de>
2022-09-30 14:44:06 +0200

    Add returnString Method + Tests

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,22 @@
-package org.example;public class Main {
+package org.example;
+
+import java.sql.Array;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+    }
+
+    public static String[] createArray() {
+        String[] numberArray = {"Eins", "Zwei", "Drei", "Vier", "Fünf", "Sechs", "Sieben", "Acht", "Neun", "Zehn"};
+        return numberArray;
+    }
+
+    public static String returnNumberString(int index) {
+        if (index != 0 && index <= 10) {
+            return createArray()[index -1];
+        }
+        return "Der Index muss zwischen 1 und 10 liegen";
     }
 }
